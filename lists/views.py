e965db6f193744fa49963f0504c3c731c04f7af1
DerGut <jannik.steinmann@gmx.de>
2018-07-09 20:25:21 +0200

    new URL + view for adding to existing lists. FT passes

--- lists/views.py
+++ lists/views.py
@@ -3,17 +3,24 @@ from django.shortcuts import render, redirect
 from lists.models import Item, List
 
 
-# TODO: Support more than one list
+# TODO: Refactor away some duplication in urls.py
+
 def home_page(request):
     return render(request, 'lists/home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'lists/list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'lists/list.html', {'list': list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item-text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item-text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
