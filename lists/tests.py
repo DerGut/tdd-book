bb8697f421be6216a3aaff5d8836c2c73e7a6442
DerGut <jannik.steinmann@gmx.de>
2018-07-09 12:30:22 +0200

    Adds some testing for another input

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,14 @@ from django.test import TestCase
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_post_request(self):
+        data = {
+            'item-text': 'A new list item'
+        }
+        response = self.client.post('/', data=data)
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'lists/home.html')
