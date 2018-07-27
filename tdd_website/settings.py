505f48c8446d2daf59963ff6c4e2fc7a28b1be5b
DerGut <jannik.steinmann@gmx.de>
2018-07-27 18:38:31 +0200

    Fixes site_name env variable

--- tdd_website/settings.py
+++ tdd_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    ALLOWED_HOSTS = [os.environ['SITE_NAME']]
 else:
     DEBUG = True
     SECRET_KEY = 'ckg4b)@w5rkqc99!+w(tujrelnd0jnxpribayktr9z1lzd=yn@'
